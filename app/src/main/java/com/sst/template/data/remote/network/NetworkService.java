c4421d1566d9278fe7877c013350e72ed6df750c
LongHV <hvlong.it@gmail.com>
2018-07-20 10:53:16 +0700

    fix conversion

--- app/src/main/java/com/sst/template/data/remote/network/NetworkService.java
+++ app/src/main/java/com/sst/template/data/remote/network/NetworkService.java
@@ -10,14 +10,10 @@ import retrofit2.http.Path;
 
 public interface NetworkService {
 
-    String GET_MOVIE_LIST = "movie/popular?api_key=2483d809cdbf68efa302d4729ad37e38";
-
-    @GET(GET_MOVIE_LIST)
+    @GET("movie/popular?api_key=2483d809cdbf68efa302d4729ad37e38")
     Single<Response<MovieListResponse>> getMovieList();
 
     @GET("movie/{movie_id}?api_key=2483d809cdbf68efa302d4729ad37e38")
     Single<Response<Movie>> getDetailMovie(@Path("movie_id") int movieId);
-
-
 
 }
